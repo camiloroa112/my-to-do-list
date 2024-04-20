e517c36cb6ce37a9f012782e98ac3360c7653c6d
camiloroa112 <camiloroa112@gmail.com>
2024-04-20 13:15:25 -0500

    Adding changes such as adding, deleting rows. Pending removal by Index.

--- adding_elements.js
+++ adding_elements.js
@@ -1,4 +1,14 @@
 function addEntries()
 {
-    
+    let table = document.getElementById('taskListTable');
+    let lastRow = parseInt(document.getElementById('taskListTable').rows.length);
+    let row = table.insertRow(lastRow);
+    let input = document.getElementById('addListInput').value;
+    let cell = row.insertCell(0);
+    cell.innerHTML = input + '&emsp;' +'<button class="btn bg-primary text-white " onclick="deleteEntries()">Add</button>';
+}
+
+function deleteEntries()
+{
+    document.getElementById("taskListTable").deleteRow();
 }
