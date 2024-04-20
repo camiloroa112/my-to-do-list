d5044d0c918622316f74ada1d5bccea7a5f11be5
camiloroa112 <camiloroa112@gmail.com>
2024-04-20 16:28:47 -0500

    Uploading final changes.

--- adding_elements.js
+++ adding_elements.js
@@ -1,14 +1,30 @@
 function addEntries()
 {
+    // Getting the Table whose ID is taskListTable
     let table = document.getElementById('taskListTable');
+
+    // Retrieving row count
     let lastRow = parseInt(document.getElementById('taskListTable').rows.length);
+    
+    // Inserting new row
     let row = table.insertRow(lastRow);
+    row.classList.add('table-info');
+    
+    // Obtaining the value from input
     let input = document.getElementById('addListInput').value;
+    
+    // Introducing value in a cell
     let cell = row.insertCell(0);
-    cell.innerHTML = input + '&emsp;' +'<button class="btn bg-primary text-white " onclick="deleteEntries()">Add</button>';
+
+    // Introducing value in new cell being what has been placed on input with a button
+    cell.innerHTML = input + '&emsp;' + '<button class="btn bg-primary text-white btn-delete">' + '<i class="fa-solid fa-trash-can"></i>' + '</button>';
+
+    // Element for deleting a specific row
+    row.querySelector('.btn-delete').addEventListener('click', deleteRow);
 }
 
-function deleteEntries()
+function deleteRow(event) 
 {
-    document.getElementById("taskListTable").deleteRow();
+    let row = event.target.closest('tr');
+    row.remove();
 }
